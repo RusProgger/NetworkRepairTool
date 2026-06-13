089f1e89ed570975d9db1b65b4c1b24aa5cd3556
RusProgger <ruslantimka92@gmail.com>
2026-06-13 13:53:40 +0300

    Add file system.h

---
+++ include/system.h
@@ -0,0 +1,18 @@
+// system.h
+
+#pragma once
+
+void releaseIp();
+void renewIp();
+void flushDns();
+void registerDns();
+void resetNetbios();
+void showRouteTable();
+void resetNetbiosReloaded();
+void fullNetworkReset();
+void resetWinsock();
+void resetTcpIp();
+void clearArpCache();
+void resetIPv6();
+void resetFirewall();
+void clearTempFiles();
