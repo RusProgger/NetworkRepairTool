3aa977a0f90c903d044fc24dffc51e76a68f74bb
RusProgger <ruslantimka92@gmail.com>
2026-06-13 11:33:41 +0300

    Update file main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,30 @@
 #include <chrono>
 #include <thread>
 #include "intro.h"
+#include "warningMessage.h"
 
 
 int main() {
 
+    // Author message
     getIntro("NetWork Repair Tool", "01/06/2026", "1.00.1", "Ruslan Voronenko");
 
+    // message running as administrator
+    if (!warningMessage()) {
+        std::cout << "\nPress Enter to exit...";
+        std::cin.get();
+        return 1;
+    } else {
 
-    // Pause;
+    }
+
+
+
+
+
+    // std::cout << "\nProgram finished successfully.\n";
+    // std::cout << "Press Enter to exit...\n";
+
+    std::cin.ignore();
     std::cin.get();
-    return 0;
 }
