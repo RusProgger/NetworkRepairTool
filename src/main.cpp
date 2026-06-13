963829f5e2e7dc2eeee70df7c33d74050e230a55
RusProgger <ruslantimka92@gmail.com>
2026-06-13 11:03:59 +0300

    Add main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-#include "mainWelcome.h"
+#include <windows.h>
+#include <chrono>
+#include <thread>
+#include "intro.h"
+
 
 int main() {
 
+    getIntro("NetWork Repair Tool", "01/06/2026", "1.00.1", "Ruslan Voronenko");
+
+
+    // Pause;
+    std::cin.get();
     return 0;
 }
